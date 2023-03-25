b1fa3a1ac33280b2d0b89c9fedb8caa11e448950
dasr4341 <59354938+dasr4341@users.noreply.github.com>
2023-03-25 21:38:55 +0530

    feat: wip auth

--- script/auth.js
+++ script/auth.js
@@ -2,9 +2,8 @@ function handleCredentialResponse(response) {
           console.log("Encoded JWT ID token: " + response.credential);
         }
         window.onload = function () {
-            google.accounts.id.initialize({
-            // client_id: "859948786595-fm85feuvhu7mn4867l493vshhfe0mr5n.apps.googleusercontent.com",
-            client_id: "166078394055-u9ncol6fjojcld3vdb21hi2mg3mkfu7r.apps.googleusercontent.com",
+          google.accounts.id.initialize({
+            client_id: "859948786595-fm85feuvhu7mn4867l493vshhfe0mr5n.apps.googleusercontent.com",
             callback: handleCredentialResponse
           });
           google.accounts.id.renderButton(
